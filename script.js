85ba687359b11c7650c07a6b91ce3818f371776c
muigai10 <gabbanajuniour@gmail.com>
2025-01-28 08:56:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Example: Form Submission Alert
+const contactForm = document.getElementById('contactForm');
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert('Thank you for contacting us! We will get back to you soon.');
+  contactForm.reset();
+});
